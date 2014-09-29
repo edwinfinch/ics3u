0c4a30bc54564f28c5f4a1a87f2d84553dce479d
Edwin Finch <contact@edwinfinch.com>
2014-09-29 14:53:37 -0400

    Add activity 6 part 1

---
+++ Activity6-part1/src/activity6/part1/Activity6Part1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package activity6.part1;
+
+/**
+ *
+ * @author edfin9913
+ */
+public class Activity6Part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+    }
+    
+}
